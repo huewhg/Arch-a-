9359664750a7ca5cb36a9c0a1b3dbe1da1dd2617
xxcosita3czxx <kokosove18@gmail.com>
2024-09-30 16:37:18 +0200

    backing up og backend

--- backend-bak/config.py
+++ backend-bak/config.py
@@ -2,8 +2,8 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-TEMPLATE_FOLDER = "../frontend/templates/"
-STATIC_FOLDER = "../frontend/static/dest/"
+TEMPLATE_FOLDER = "../frontend/templates"
+STATIC_FOLDER = "../frontend/static/dest"
 TEMPLATES_AUTO_RELOAD = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'project.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,6 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from pathlib import Path
-import config
-
 
 app = FastAPI()
 
@@ -13,8 +11,7 @@ templates = Jinja2Templates(directory=str(Path(__file__).parent / "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse(f"{config.TEMPLATE_FOLDER}", {"request": request, "title": "Hello FastAPI!"})
-
+    return "{data:'online'}"
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
